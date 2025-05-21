9620b5052e2dc036c7ee440499771323b9fa96c2
Roberto Alves <roberto_junior_sousa@hotmail.com>
2025-05-21 00:20:28 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   const handleCount = () => {
     setCount((prev) => prev + 1);
-  };
+  }
 
   useEffect(() => {
-    window.dispatchEvent(new CustomEvent("addItem", { detail: count }));
+    window.dispatchEvent(new CustomEvent('addItem', { detail: count }));
   }, [count]);
 
   return (
     <>
       <h1>MFE 1</h1>
       <div className="card">
-        <button className="btn btn-warning" onClick={handleCount}>
+        <button className='btn btn-warning' onClick={handleCount}>
           count is {count}
         </button>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
